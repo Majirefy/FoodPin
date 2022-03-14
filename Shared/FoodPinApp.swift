a18bb8d5b3a28033df6941bfd2879b0b47c47d5a
Majirefy <song.fc@outlook.com>
2022-03-14 19:26:43 +0800

    Initial Commit

---
+++ Shared/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  Shared
+//
+//  Created by Fangcheng Song on 2022/3/14.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
